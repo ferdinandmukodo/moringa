4fd26b7528ccb63c4c98f5f995f7a5c209757434
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-07-13 10:27:03 +0300

    js code

---
+++ index.js
@@ -0,0 +1,9 @@
+var birds = ["Parrots","Owls","Penguins"];
+var reptiles = ["Lizards","Turtles","Dinosaur"];
+var amphipians = ["Frogs","Salamanda","Caudata"];
+var mammals = ["Humans","Elephants","sloths"];
+//birds.concat(mammals);
+//console.log(birds)
+//console.log(mammals[])
+var movies = ["Avengers","Batman","Black Panther"]
+console.log(movies.indexOf(Batman)) 
